830df5c663fbe93c6e4cc158c73b4a851f713424
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2024-07-20 22:32:05 +0900

    [Silver I] Title: 그림, Time: 380 ms, Memory: 37280 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+    static int N, M, max, cnt;
+    static Queue<Point> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        queue = new ArrayDeque<>();
+        max = 0;
+        cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    cnt++;
+                    bfs(i, j);
+                }
+            }
+        }
+        System.out.println(cnt);
+        System.out.println(max);
+    }
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void bfs(int i, int j) {
+        queue.add(new Point(i, j));
+        visited[i][j] = true;
+        int space = 0;
+        while (!queue.isEmpty()) {
+            space++;
+            Point cur = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && map[nx][ny] == 1) {
+                    queue.add(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+        max = Math.max(max, space);
+    }
+
+    public static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
