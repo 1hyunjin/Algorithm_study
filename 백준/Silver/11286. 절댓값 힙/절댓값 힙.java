ddaf33f3e43f378fb7eca9817f16290c6aa9461f
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2024-11-22 10:22:54 +0900

    [Silver I] Title: 절댓값 힙, Time: 244 ms, Memory: 24440 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,34 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
 
-        Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
-            if (Math.abs(o1) > Math.abs(o2)) {
-                return 1;
-            }
-            else if(Math.abs(o1) < Math.abs(o2)) {
-                return -1;
-            }
-            else {
-                return Integer.compare(o1, o2);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2){
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }
+                return Math.abs(o1) - Math.abs(o2);
             }
         });
 
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
-
-            if (x != 0) {
-                pq.add(x);
-            }else {
-                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                pq.offer(num);
+            } else {
+                if (pq.isEmpty()) {
+                    sb.append(0).append('\n');
+                }
+                else {
+                    sb.append(pq.poll()).append('\n');
+                }
             }
         }
         System.out.println(sb.toString());
