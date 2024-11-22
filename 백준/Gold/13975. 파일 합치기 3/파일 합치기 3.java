42a5b639e6ce1414febe6f893a74c78238eab696
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2024-11-22 13:53:01 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2432 ms, Memory: 340460 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -9,25 +9,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < T; tc++) {
-            int K = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
+        for (int t = 0; t < T; t++) {
+
             PriorityQueue<Long> pq = new PriorityQueue<>();
-            for (int i = 0; i < K; i++) {
+
+            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
                 pq.offer(Long.parseLong(st.nextToken()));
             }
+
             long answer = 0;
-            while (true) {
-                if (pq.size() == 1) {
-                    break;
-                }
+
+            while (pq.size() > 1) {
                 long num1 = pq.poll();
                 long num2 = pq.poll();
                 answer += num1 + num2;
-                pq.offer(num1 + num2);
+                pq.offer(num1+num2);
             }
+
             System.out.println(answer);
         }
-
     }
 }
