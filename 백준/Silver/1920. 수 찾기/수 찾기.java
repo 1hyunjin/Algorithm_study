1c6d3245050cf62e82b1de71613933c57db7987f
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-01-21 16:17:53 +0900

    [Silver IV] Title: 수 찾기, Time: 504 ms, Memory: 60768 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,48 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
     public static void main(String[] args) throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
 
-        arr = new int[N];
+        int N = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            map.put(Integer.parseInt(st.nextToken()), 0);
         }
 
-        Arrays.sort(arr);
-
-        M = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
-            System.out.println(binarySearch(num));
-        }
-    }
-
-    public static int binarySearch(int target) {
-        int start = 0;
-        int end = N-1;
-
-        while (start <= end) {
-            int mid = (start + end)/2;
-            if (arr[mid] > target) {
-                end = mid - 1;
-            } else if (arr[mid] < target) {
-                start = mid + 1;
-            } else {
-                return 1;
+            if (map.containsKey(num)) {
+                sb.append(1).append("\n");
+            }
+            else {
+                sb.append(0).append("\n");
             }
         }
-        return 0;
+        System.out.println(sb);
     }
 }
