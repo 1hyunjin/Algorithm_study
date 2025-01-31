0ab65df4c1da5b86ba72f559ad57113796fb8256
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-01-31 15:19:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        while (N > 0) {
+
+            if (N % 5 == 0) {
+                cnt += N/5;
+                break;
+            } else if (N < 3) {
+                cnt = -1;
+                break;
+            }
+            else {
+                N -= 3;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
