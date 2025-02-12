35a3bc8f99c3c95fbf21ae06d07fe1bcc1e401e5
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-02-12 13:46:19 +0900

    [Silver III] Title: N과 M (7), Time: 844 ms, Memory: 407804 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        result = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        sb = new StringBuilder();
+
+        comb(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void comb(int idx, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            result[idx] = arr[i];
+            comb(idx+1, cnt+1);
+        }
+    }
+}
