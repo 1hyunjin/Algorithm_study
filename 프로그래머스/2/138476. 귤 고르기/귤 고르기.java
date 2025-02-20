a542a0899ebea80a6cc3da9f4344eeafff6e7119
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-02-20 09:37:15 +0900

    [level 2] Title: 귤 고르기, Time: 341.77 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int len = tangerine.length;
+        Arrays.sort(tangerine);
+        
+        Point[] point = new Point[10000001];
+        
+        Arrays.fill(point, new Point(0,0));
+        
+        for(int i = 0; i < len; i++){
+            int n = tangerine[i];
+            if(point[n].cnt == 0){
+                point[n] = new Point(n, 1);
+            }
+            else {
+                point[n] = new Point(n, point[n].cnt+1);
+            }
+        }
+        
+        Arrays.sort(point);
+        
+        int type = 0;
+        int total = 0; 
+        
+        for(int i = 0; i < point.length; i++){
+            
+            k -= point[i].cnt;
+            total++;
+            
+            if(k <= 0){
+                answer = total;
+                break;
+            }
+        }
+        return answer;
+    }
+    
+    public static class Point implements Comparable<Point>{
+        int num;
+        int cnt;
+        
+        public Point(int num, int cnt){
+            this.num = num;
+            this.cnt = cnt;
+        }
+        
+        @Override
+        public int compareTo(Point o){
+            return o.cnt - this.cnt;
+        }
+    }
+}
