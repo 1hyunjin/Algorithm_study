0d7c0cff547757504b79736684224199d6512095
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-02-26 14:49:58 +0900

    [Gold III] Title: 감시, Time: 432 ms, Memory: 49224 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,237 +1,194 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[][] map;
+    static int n, m;
+    static List<CCTV> cctvList;
+    static int[] result;
+    static int min;
+    static int[][] copyMap;
 
-	static int N, M;
-	static int[][] map;
-	static List<CCTV> cctvList;
-	static int[] result;
-	static int min;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        map = new int[n][m];
+        cctvList = new ArrayList<>();
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
 
-		map = new int[N][M];
-		cctvList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] != 0 && map[i][j] != 6) {
+                    cctvList.add(new CCTV(i, j, map[i][j]));
+                }
+            }
+        }
+        min = Integer.MAX_VALUE; // 0의 개수
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				// 0(빈칸)이 아니고 6(벽) 이 아니면 cctv 들이니까 모아두자.
-				if (map[i][j] != 0 && map[i][j] != 6) {
-					cctvList.add(new CCTV(i, j, map[i][j]));
-				}
-			}
-		}
-		// end input
-		result = new int[cctvList.size()];
-		min = Integer.MAX_VALUE;
-		subset(0);
-		
-		System.out.println(min);
-	}
+        result = new int[cctvList.size()];
+        subset(0);
 
-	public static void subset(int idx) {
+        System.out.println( min );
+    }
 
-		if (idx == cctvList.size()) {
-//			System.out.println(Arrays.toString(result));
-			// 이제 상태대로 돌려야하는뎁
-			checkStatus(result);
-			return;
-		}
+    public static void subset(int cnt) {
 
-		for (int s = 1; s <= 4; s++) {
-			// cctv 2번은 상태가 2번까지만 존재
-			if (s == 3) {
-				if (cctvList.get(idx).num == 2) {
-					break;
-				}
-			}
-			// cctv 5번은 상태가 1번까지만 존재
-			if (s == 2) {
-				if (cctvList.get(idx).num == 5) {
-					break;
-				}
-			}
-			result[idx] = s;
-			subset(idx + 1);
-		}
-	}
+        if(cnt == cctvList.size()){
+            // 이 result 배열을 가지고 감시하는 곳 봐야겠지??
+            check(result);
+            return;
+        }
 
-	static int[][] copyMap;
+        for (int d = 1; d <= 4; d++) {
+            if (d == 2) {
+                if (cctvList.get(cnt).num == 5) {
+                    break;
+                }
+            }
+            if (d == 3) {
+                if (cctvList.get(cnt).num == 2) {
+                    break;
+                }
+            }
+            result[cnt] = d;
+            subset(cnt+1);
+        }
+    }
 
-	public static void checkStatus(int[] result) {
+    public static void check(int[] result) {
+        copyMap = new int[n][m];
+        deepCopy(map);
+        int cnt = 0;
 
-		copyMap = new int[N][M];
-		deepCopy(map);
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == 1) {
+                right(cctvList.get(i));
 
-		for (int i = 0; i < result.length; i++) {
-			// 상태가 1일 때 공통적으로 오른쪽을 향함.
-			if (result[i] == 1) {
-				right(cctvList.get(i));
+                if (cctvList.get(i).num == 2 || cctvList.get(i).num == 4 || cctvList.get(i).num == 5) {
+                    left(cctvList.get(i));
+                }
+                if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4 || cctvList.get(i).num == 5) {
+                    up(cctvList.get(i));
+                }
+                if (cctvList.get(i).num == 5) {
+                    down(cctvList.get(i));
+                }
+            }
+            if (result[i] == 2) {
+                down(cctvList.get(i));
+                if (cctvList.get(i).num == 2 || cctvList.get(i).num == 4 ) {
+                    up(cctvList.get(i));
+                }
+                if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4 ) {
+                    right(cctvList.get(i));
+                }
+            }
+            if (result[i] == 3) {
+                left(cctvList.get(i));
+                if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4 ) {
+                    down(cctvList.get(i));
+                }
+                if (cctvList.get(i).num == 4) {
+                    right(cctvList.get(i));
+                }
+            }
+            if (result[i] == 4) {
+                up(cctvList.get(i));
+                if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4 ) {
+                    left(cctvList.get(i));
+                }
+                if (cctvList.get(i).num == 4) {
+                    down(cctvList.get(i));
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copyMap[i][j] == 0) {
+                    cnt++;
+                }
+            }
+        }
+        min = Math.min(min, cnt);
+    }
 
-				if (cctvList.get(i).num == 2 || cctvList.get(i).num == 4 || cctvList.get(i).num == 5) {
-					left(cctvList.get(i));
-				}
-				if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4 || cctvList.get(i).num == 5) {
-					up(cctvList.get(i));
-				}
-				if (cctvList.get(i).num == 5) {
-					down(cctvList.get(i));
-				}
-			}
-			if (result[i] == 2) {
-				down(cctvList.get(i));
+    public static void right(CCTV cctv) {
+        int ny = cctv.y + 1;
+        while (ny < m) {
+            if (copyMap[cctv.x][ny] == 6) {
+                break;
+            }
+            if (copyMap[cctv.x][ny] == 0) {
+                copyMap[cctv.x][ny] = 7;
+            }
+            ny++;
+        }
+    }
+    public static void left(CCTV cctv) {
+        int ny = cctv.y - 1;
+        while (ny >= 0) {
+            if (copyMap[cctv.x][ny] == 6) {
+                break;
+            }
+            if (copyMap[cctv.x][ny] == 0) {
+                copyMap[cctv.x][ny] = 7;
+            }
+            ny--;
+        }
+    }
+    public static void up(CCTV cctv) {
+        int nx = cctv.x - 1;
+        while (nx >= 0) {
+            if (copyMap[nx][cctv.y] == 6) {
+                break;
+            }
+            if (copyMap[nx][cctv.y] == 0) {
+                copyMap[nx][cctv.y] = 7;
+            }
+            nx--;
+        }
+    }
+    public static void down(CCTV cctv) {
+        int nx = cctv.x + 1;
+        while (nx < n) {
+            if (copyMap[nx][cctv.y] == 6) {
+                break;
+            }
+            if (copyMap[nx][cctv.y] == 0) {
+                copyMap[nx][cctv.y] = 7;
+            }
+            nx++;
+        }
+    }
 
-				if (cctvList.get(i).num == 2 || cctvList.get(i).num == 4) {
-					up(cctvList.get(i));
-				}
+    public static void deepCopy(int[][] map) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                copyMap[i][j] = map[i][j];
+            }
+        }
+    }
 
-				if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4) {
-					right(cctvList.get(i));
-				}
-			}
-			if (result[i] == 3) {
-				left(cctvList.get(i));
+    public static class CCTV{
+        int x;
+        int y;
+        int num;
 
-				if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4) {
-					down(cctvList.get(i));
-				}
-				if (cctvList.get(i).num == 4) {
-					right(cctvList.get(i));
-				}
-			}
-			if (result[i] == 4) {
-				up(cctvList.get(i));
+        public CCTV(int x, int y, int num) {
+            this.x = x;
+            this.y = y;
+            this.num = num;
+        }
 
-				if (cctvList.get(i).num == 3 || cctvList.get(i).num == 4) {
-					left(cctvList.get(i));
-				}
-				if (cctvList.get(i).num == 4) {
-					down(cctvList.get(i));
-				}
-
-			}
-		}
-		
-//		for(int i = 0; i < N; i++) {
-//			for(int j =0; j < M; j++) {
-//				System.out.print(copyMap[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
-//		System.out.println("============================");
-		
-		int cnt = 0;
-		for(int i = 0; i< N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(copyMap[i][j] == 0) {
-					cnt++;
-				}
-			}
-		}
-		min = Math.min(min, cnt);
-	}
-
-	public static void right(CCTV cctv) {
-
-		int ny = cctv.j+1;
-
-		while (ny < M) {
-			
-			if (copyMap[cctv.i][ny] == 6) {
-				break;
-			}
-			// 빈칸이면
-			if (copyMap[cctv.i][ny] == 0) {
-				copyMap[cctv.i][ny] = 7;
-			}
-			ny++;
-		}
-	}
-
-	public static void left(CCTV cctv) {
-
-		int ny = cctv.j-1;
-
-		while (ny >= 0) {
-			
-			if (copyMap[cctv.i][ny] == 6) {
-				break;
-			}
-			// 빈칸이면
-			if (copyMap[cctv.i][ny] == 0) {
-				copyMap[cctv.i][ny] = 7;
-			}
-			ny--;
-		}
-	}
-	public static void up(CCTV cctv) {
-
-		int nx = cctv.i-1;
-
-		while (nx >= 0) {
-			
-			if (copyMap[nx][cctv.j] == 6) {
-				break;
-			}
-			// 빈칸이면
-			if (copyMap[nx][cctv.j] == 0) {
-				copyMap[nx][cctv.j] = 7;
-			}
-			nx--;
-		}
-	}
-	public static void down(CCTV cctv) {
-
-		int nx = cctv.i+1;
-
-		while (nx < N) {
-			
-			if (copyMap[nx][cctv.j] == 6) {
-				break;
-			}
-			// 빈칸이면
-			if (copyMap[nx][cctv.j] == 0) {
-				copyMap[nx][cctv.j] = 7;
-			}
-			nx++;
-		}
-	}
-
-	public static void deepCopy(int[][] map) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				copyMap[i][j] = map[i][j];
-			}
-		}
-	}
-
-	public static class CCTV {
-		int i, j;
-		int num; // cctv 번호
-
-		public CCTV(int i, int j, int num) {
-			this.i = i;
-			this.j = j;
-			this.num = num;
-		}
-
-		@Override
-		public String toString() {
-			return "CCTV [i=" + i + ", j=" + j + ", num=" + num + "]";
-		}
-	}
+        @Override
+        public String toString() {
+            return "{CCTV :" + num + "번 CCTV" + this.x + " , " + this.y + "}";
+        }
+    }
 }
