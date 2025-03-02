12159fa94ede7b97d870e8d048edcb4ff3047690
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-03-02 19:29:53 +0900

    [Silver III] Title: N과 M (1), Time: 1700 ms, Memory: 73392 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[] num;
-    static int N, M;
+    static int n, m;
     static boolean[] isSelected;
-    static int[] result;
-
-    public static void main(String[] args) throws IOException {
+    static int[] nums;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        num = new int[N];
-        for(int i = 0; i < N; i++){
-            num[i] = i+1;
-        }
-        isSelected = new boolean[N];
-        result = new int[M];
+        isSelected = new boolean[n+1];
+        nums = new int[m];
 
         perm(0);
     }
 
     public static void perm(int cnt) {
-        if (cnt == M) {
-            for (int i = 0; i < result.length; i++) {
-                System.out.print(result[i] + " ");
+
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(nums[i] + " ");
             }
             System.out.println();
             return;
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= n; i++) {
             if (!isSelected[i]) {
                 isSelected[i] = true;
-                result[cnt] = num[i];
+                nums[cnt] = i;
                 perm(cnt+1);
                 isSelected[i] = false;
             }
