b71e43480f18a8254f64e304bbea5298cc9d8ff4
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-03-02 20:53:12 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,41 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M;
+    static int n, m;
     static boolean[] isSelected;
+    static int[] nums;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        isSelected = new boolean[N];
-
+        isSelected = new boolean[n];
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i+1;
+        }
         comb(0, 0);
     }
 
-    public static void comb(int idx, int cnt) {
+    public static void comb(int cnt, int start) {
 
-        if (cnt == M) {
-            for (int i = 0; i < N; i++) {
+        if (cnt == m) {
+            for (int i = 0; i < n; i++) {
                 if (isSelected[i]) {
-                    System.out.print(i + 1 + " ");
+                    System.out.print(nums[i] + " ");
                 }
             }
             System.out.println();
             return;
         }
 
-        if (idx == N) {
+        if (start == n) {
             return;
         }
 
-        isSelected[idx] = true;
-        comb(idx+1, cnt+1);
-        isSelected[idx] = false;
-        comb(idx + 1, cnt);
+        isSelected[start] = true;
+        comb(cnt+1, start+1);
+        isSelected[start] = false;
+        comb(cnt, start+1);
     }
 }
