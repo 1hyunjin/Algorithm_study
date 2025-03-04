ff7f41ac85963dfdd7d8a65dbfb4fa807ab05e9a
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-03-04 09:35:07 +0900

    [level 2] Title: 피보나치 수, Time: 2.98 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,22 +1,21 @@
-import java.util.Arrays;
-class Solution {
-    public int solution(int n) {
-        Arrays.fill(mem, -1);
-        for (int i = 0; i <= n; i++) {
-            fibonacci(i);
-        }
-        System.out.println(fibonacci(n));
-        return fibonacci(n);
-    }
-    public static final int[] mem = new int[100001];
+import java.util.*;
+import java.io.*;
 
-    public static int fibonacci(int n) {
-        if (mem[n] != -1) {
-            return mem[n];
+class Solution {
+    
+    static int[] dp;
+    
+    public int solution(int n) {
+        int answer = 0; 
+        dp = new int[100001];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
-        if (n == 0 || n == 1) {
-            return n;
-        }
-        return mem[n] = (fibonacci(n-1) + fibonacci(n-2)) % 1234567;
+        
+        answer = dp[n];
+        return answer;
     }
 }
