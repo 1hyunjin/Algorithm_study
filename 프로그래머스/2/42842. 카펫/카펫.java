f2ea89165aa748cf0eddf3cd91c183fa6dfb810a
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-03-20 09:32:58 +0900

    [level 2] Title: 카펫, Time: 22.37 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        for(int c = 3; c <= 2500; c++){
+            for(int r = 3; r <= c; r++){
+                int edge = 2*c + 2*r - 4; 
+                
+                if(edge == brown && (r*c)-edge == yellow){
+                    answer[0] = c;
+                    answer[1] = r;
+                }
+            }
+        }
+        return answer;
+    }
+}
