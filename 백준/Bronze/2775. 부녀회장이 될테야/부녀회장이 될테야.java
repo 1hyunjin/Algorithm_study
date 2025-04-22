dd87e9cde9a02046d8073a731460886983b55081
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-04-22 14:54:42 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 64 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,31 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int TC = Integer.parseInt(br.readLine());
-
-        for (int tc = 0; tc < TC; tc++) {
-            int k = Integer.parseInt(br.readLine());
-            int n = Integer.parseInt(br.readLine());
-
-            int[][] dp = new int[k+1][n+1];
-
-            for (int i = 1; i <= n; i++) {
-                dp[0][i] = i;
-            }
-
-            for (int i = 1; i <= k; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (j == 1) {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                    else {
-                        dp[i][j] = dp[i][j-1] + dp[i-1][j];
-                    }
+        int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j <15; j++) {
+                if (j == 1) {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
                 }
             }
+        }
+        for (int tc = 0; tc < T; tc++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
             System.out.println(dp[k][n]);
         }
     }
