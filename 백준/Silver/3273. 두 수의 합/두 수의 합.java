64d3769715bf299567a4809c2f961cd5bcc9f9c0
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-04-25 20:43:56 +0900

    [Silver III] Title: 두 수의 합, Time: 188 ms, Memory: 25204 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,35 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    // O(N^2)알고리즘 -> O(N) 으로도 풀 수 있다.
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         int x = Integer.parseInt(br.readLine());
 
-        int[] arr1 = new int[x];
-        int result = 0;
-
-        //나와 합이 x가 되는 원소를 매 순간마다 O(1)에 찾고 이 행위를 N번 반복하기 때문에 총 시간복잡도는 O(N)이 된다.
+        int[] nums = new int[x];
+        int cnt = 0;
         for (int i = 0; i < N; i++) {
             if (x - arr[i] > 0) {
-                arr1[arr[i]] = 1;
-                // n개의 서로 다른 양의 정수이기 때문에
-                if (arr[i] != x-arr[i] && arr1[x - arr[i]] == 1) {
-//                    System.out.println(arr[i] + " , " + (x-arr[i]));
-                    result++;
-                }
+                nums[arr[i]] = 1;
+                int diff = x - arr[i];
+
+                if (diff != arr[i] && nums[diff] == 1) {
+                    cnt++;
+                } 
             }
         }
-        System.out.println(result);
+        System.out.println(cnt);
     }
 }
