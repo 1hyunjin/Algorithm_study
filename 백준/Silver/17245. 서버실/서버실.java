6c9f5878c6d7ab5e2b4f158a61c907e2d2d5d925
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-04-30 15:58:44 +0900

    [Silver II] Title: 서버실, Time: 580 ms, Memory: 125168 KB -BaekjoonHub

---
+++ 백준/Silver/17245. 서버실/서버실.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        int[][] map = new int[N][N];
+        long end = 0;
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                end = Math.max(end, map[i][j]);
+                sum += map[i][j];
+            }
+        }
+
+        long start = 0;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][j] <= mid) {
+                        cnt += map[i][j];
+                    } else {
+                        cnt += mid;
+                    }
+                }
+            }
+            if (cnt*2 >= sum) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.println(start);
+
+    }
+}
