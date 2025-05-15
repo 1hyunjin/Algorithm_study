e1c6a0eb7efb502bb3102aff6ec80894c4da8402
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-05-15 21:54:02 +0900

    [Gold V] Title: 수 고르기, Time: 248 ms, Memory: 23152 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,37 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         int[] arr = new int[N];
-
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
         Arrays.sort(arr);
-
-        int start = 0;
-        int end = 0;
+        int s = 0;
+        int e = 0;
         int min = Integer.MAX_VALUE;
-        while (end < N) {
-            int diff = arr[end] - arr[start];
-
+        while (e < N) {
+            int diff = arr[e] - arr[s];
+            if (diff < M) {
+                e++;
+            }
             if (diff >= M) {
                 min = Math.min(min, diff);
-                start++;
+                s++;
             }
             if (diff == M) {
                 break;
-            }
-            if (diff <= M) {
-                end++;
             }
         }
         System.out.println(min);
