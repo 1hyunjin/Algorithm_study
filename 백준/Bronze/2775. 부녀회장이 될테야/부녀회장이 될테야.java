05ea60361f5b257aa64ecc4eeec5828da61ae2f1
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-05-16 09:12:18 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 64 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -4,22 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int[][] dp = new int[15][15];
-        for (int i = 0; i < 15; i++) {
+        int TC = Integer.parseInt(br.readLine());
+        int[][]dp = new int[15][15];
+        for (int i = 1; i <= 14; i++) {
             dp[0][i] = i;
         }
-        for (int i = 1; i < 15; i++) {
-            for (int j = 1; j <15; j++) {
+        for (int i = 1; i <= 14; i++) {
+            for (int j = 1; j <= 14; j++) {
                 if (j == 1) {
                     dp[i][j] = dp[i-1][j];
                 }
-                else {
+                else{
                     dp[i][j] = dp[i][j-1] + dp[i-1][j];
                 }
             }
         }
-        for (int tc = 0; tc < T; tc++) {
+        for (int t = 0; t < TC; t++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
             System.out.println(dp[k][n]);
