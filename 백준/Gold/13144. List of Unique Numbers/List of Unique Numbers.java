9f462d759b7f06df772759e1766636688dcd7d70
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-05-16 16:39:22 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 192 ms, Memory: 26100 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,36 +1,30 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
-        int max = 0;
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(arr[i], max);
         }
+        int[] num = new int[100001];
 
-        int[] used = new int[max + 1];
-
-        int right = 0;
-        int left = 0;
+        int s =0 ;
+        int e = 0;
         long cnt = 0;
 
-        while (right < N) {
-            if(used[arr[right]] == 0) {
-                used[arr[right]] = 1;
-                right++;
-                cnt+= right - left;
+        while (e < N) {
+            if (num[arr[e]] == 0) {
+                num[arr[e]]++;
+                e++;
+                cnt += e-s;
             }
-            else if (used[arr[right]] == 1 ) {
-                used[arr[left]] = 0;
-                left++;
+            else{
+                num[arr[s]]--;
+                s++;
             }
         }
         System.out.println(cnt);
