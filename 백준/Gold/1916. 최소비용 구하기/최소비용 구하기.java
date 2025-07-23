5f79714258a2dd7420bb189fa395f59588277f50
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-07-23 16:52:03 +0900

    [Gold V] Title: 최소비용 구하기, Time: 336 ms, Memory: 50268 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -5,62 +5,57 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int V = Integer.parseInt(br.readLine());
-        int E = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 도시 개수
+        int M = Integer.parseInt(br.readLine()); // 버스 개수
 
-        List<Point>[] graph = new List[V + 1];
-        for (int i = 0; i <= V; i++) {
+        List<Point>[] graph = new List[N+1];
+        for (int i = 0; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-        int[] arr = new int[V + 1];
-
+        int[] arr = new int[N+1];
         Arrays.fill(arr, Integer.MAX_VALUE);
 
-        for (int i = 0; i < E; i++) {
+        StringTokenizer st;
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Point(w, v));
+            int dist = Integer.parseInt(st.nextToken());
+            graph[u].add(new Point(v, dist));
         }
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
+        arr[start] = 0;
         PriorityQueue<Point> pq = new PriorityQueue<>(new Comparator<Point>(){
             @Override
             public int compare(Point o1, Point o2) {
                 return o1.dist - o2.dist;
             }
         });
-        boolean[] visited = new boolean[V+1];
-
-        pq.add(new Point(0, start));
-        arr[start] = 0;
-
+        pq.offer(new Point(start, 0));
         while (!pq.isEmpty()) {
             Point cur = pq.poll();
-            if (!visited[cur.v]) {
-                visited[cur.v] = true;
-                for (Point p : graph[cur.v]) {
-                    if (arr[p.v] > arr[cur.v] + p.dist) {
-                        arr[p.v] = arr[cur.v] + p.dist;
-                        pq.add(new Point(arr[p.v], p.v));
-                    }
+            if (cur.v == end) {
+                System.out.println(arr[end]);
+                break;
+            }
+            for(Point next: graph[cur.v]){
+                if (cur.dist + next.dist < arr[next.v]) {
+                    arr[next.v] = cur.dist+next.dist;
+                    pq.offer(new Point(next.v, arr[next.v]));
                 }
             }
         }
-        System.out.println(arr[end]);
     }
-
-    public static class Point {
-        int dist;
+    public static class Point{
         int v;
+        int dist;
 
-        public Point(int dist, int v) {
-            this.dist = dist;
+        public Point(int v, int dist) {
             this.v = v;
+            this.dist = dist;
         }
     }
 }
