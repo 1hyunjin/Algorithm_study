391812c5d6a1495b7566037309a3ebd747b30428
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-07-25 09:14:53 +0900

    [level 2] Title: 방문 길이, Time: 15.89 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 85.5 MB, 시간: 17.80 ms
+메모리: 79.4 MB, 시간: 15.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 01일 14:21:52
+2025년 07월 25일 09:14:50
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -9,44 +9,34 @@ class Solution {
         for(char c : dirs.toCharArray()){
             int nx = x;
             int ny = y;
-            
-            String oppsite = "";
-            
+                        
             if(c == 'U'){
-                oppsite = "D";
                 ny++;
             } else if(c == 'D'){
-                oppsite = "U";
                 ny--;
             } else if(c == 'R'){
-                oppsite = "L";
                 nx++;
             } else if(c == 'L'){
-                oppsite = "R";
                 nx--;
             }
             // 경계값을 넘으면 pass
             if(ny < -5 || ny > 5 || nx < -5 || nx > 5) {
                 continue;
             }
-            // 반대로 길을 지난 경우 
-            String before = oppsite+x+y;
-            // 똑바로 길을 지난 경우 
-            String after = String.valueOf(c) + nx + ny;
-            // x, y 좌표 갱신
+            String cur = x+","+y+"->"+nx+","+ny;
+            String reverse = nx+","+ny+"->"+x+","+y;
+            
             x = nx;
             y = ny;
-            //똑바로, 반대로 길을 지난 경우면 pass
-            if(set.contains(before) || set.contains(after)){
+            
+            if(set.contains(cur) || set.contains(reverse)){
                 continue;
             }
-            set.add(before);
-            set.add(after);
+            set.add(cur);
+            set.add(reverse);
             
-            // 처음 지난 길 개수 +1 증가
             cnt++;
         }
-        // 정답 처리
         return cnt;
     }
 }
