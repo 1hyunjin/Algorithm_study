8cdbb137e1aecc451e97bbc8f60cb1558f2421db
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-07-28 17:27:18 +0900

    [Gold IV] Title: 부분합, Time: 180 ms, Memory: 22608 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,50 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N+1];
-
+        long[] arr = new long[N + 1];
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
         }
-
         int left = 0;
         int right = 0;
-
-        int min = Integer.MAX_VALUE;
-        int tot = 0;
-
-        while (left <= right && right <= N) {
-            // S 보다 작으면 right 이동
-            if (tot < S) {
-                tot += arr[right];
+        int len = Integer.MAX_VALUE;
+        while (right < N + 1) {
+            long diff = arr[right]-arr[left];
+            if (diff < S) {
                 right++;
             }
-            // S랑 같거나 크다면 길이 최소 구하고 left 이동
-            else if (tot >= S) {
-                min = Math.min(min, right - left);
-                tot -= arr[left];
+            else {
+                len = Math.min(len, right-left);
                 left++;
             }
         }
-        // 합을 찾지 못한다면
-        if (min == Integer.MAX_VALUE) {
-            System.out.println(0);
-        }
-        else {
-            System.out.println(min);
-        }
+        System.out.println(len==Integer.MAX_VALUE?0:len);
     }
 }
