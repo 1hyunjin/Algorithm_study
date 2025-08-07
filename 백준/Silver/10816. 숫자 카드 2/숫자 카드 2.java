bebe4dedc4c49ea759443d37c8d4847abe75ffbd
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-08-07 13:27:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 948 ms, Memory: 242896 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,69 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
-
-        M = Integer.parseInt(br.readLine());
-
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
-            sb.append(upperBound(num) - lowerBound(num)).append(" ");
+            if (map.containsKey(num)) {
+                sb.append(map.get(num)).append(" ");
+            }
+            else{
+                sb.append(0).append(" ");
+            }
         }
-
         System.out.println(sb);
-    }
-
-    public static int lowerBound(int target) {
-        int lo = 0;
-        int hi = N;
-
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-
-            if (target <= arr[mid]) {
-                hi = mid;
-            }
-            else {
-                lo = mid + 1;
-            }
-        }
-        return lo;
-    }
-
-    public static int upperBound(int target) {
-        int lo = 0;
-        int hi = N;
-
-        while (lo < hi) {
-            int mid = (lo + hi) /2;
-
-            if (target < arr[mid]) {
-                hi = mid;
-            }
-            else {
-                lo = mid + 1;
-            }
-        }
-
-        return lo;
     }
 }
