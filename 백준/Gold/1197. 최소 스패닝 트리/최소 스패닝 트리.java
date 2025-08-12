e9ee138cdb98f6a30bc95d84d57fcc071c741073
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-08-12 16:30:03 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 464 ms, Memory: 51116 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,88 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int V, E;
-    static Edge[]  graph;
     static int[] parent;
-    static int result;
-
+    static int V, E;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-
-        graph = new Edge[E];
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            graph[i] = new Edge(from, to, weight);
-        }
-        Arrays.sort(graph);
-
-        // make set
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>(){
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.w - o2.w;
+            }
+        });
         parent = new int[V+1];
-        for (int i = 1; i <= V; i++) {
+        for (int i = 0; i <= V; i++) {
             parent[i] = i;
         }
-
-        result = 0;
-        int cnt = 0;
-        for (Edge edge : graph) {
-            if (union(edge.from, edge.to)) {
-                continue;
-            }
-            result += edge.weight; // 가중치 누적
-            if (++cnt == V - 1) {
-                break;
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            pq.offer(new Node(a, b, w));
+        }
+        int result = 0;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (find(cur.a) != find(cur.b)) {
+                union(cur.a, cur.b);
+                result+=cur.w;
             }
         }
         System.out.println(result);
     }
+    public static class Node{
+        int a;
+        int b;
+        int w;
 
-    public static int find(int a) {
-        if (a == parent[a]) {
-            return a;
+        public Node(int a, int b, int w) {
+            this.a = a;
+            this.b = b;
+            this.w = w;
         }
-        return parent[a] = find(parent[a]);
     }
 
-    public static boolean union(int a, int b) {
-        int aRoot = find(a);
-        int bRoot = find(b);
+    public static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
 
-        if(aRoot == bRoot) {
-            return true;
+        if (rootA < rootB) {
+            parent[rootB] = rootA;
         }
-        if (aRoot < bRoot) {
-            parent[bRoot] = aRoot;
-        } else {
-            parent[aRoot] = bRoot;
+        else{
+            parent[rootA] = rootB;
         }
-        return false;
     }
-
-    public static class Edge implements Comparable<Edge> {
-        int from, to, weight;
-
-        public Edge(int from, int to, int weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
+    public static int find(int x){
+        if (x == parent[x]) {
+            return x;
         }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.weight - o.weight;
-        }
+        return parent[x] = find(parent[x]);
     }
 }
