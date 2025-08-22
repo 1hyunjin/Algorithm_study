ea08defcb8f0025a278d66ee477947fa0d7a739c
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-08-22 15:56:26 +0900

    [Gold III] Title: 파티, Time: 560 ms, Memory: 56956 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -10,56 +10,48 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-
         graph = new List[N+1];
-
         for (int i = 0; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph[u].add(new Point(v, w));
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
+            graph[s].add(new Point(e, dist));
         }
-        int[] back = func(X); // 돌아오는거
+        int[] arr = dikstra(X);
         int max = 0;
         for (int i = 1; i <= N; i++) {
-            if(i == X) continue;
-
-            int[] go = func(i);
-            int total = go[X] + back[i];
-            max = Math.max(max, total);
+            if(i==X) continue;
+            int[] tmp  = dikstra(i);
+            int sum = arr[i] + tmp[X];
+            max = Math.max(max, sum);
         }
-
         System.out.println(max);
     }
 
-    public static int[] func(int start) {
-        int[] arr = new int[N+1];
-        Arrays.fill(arr, Integer.MAX_VALUE);
-        boolean[] visited = new boolean[N+1];
-
-        PriorityQueue<Point> backPq = new PriorityQueue<>(new Comparator<Point>(){
+    public static int[]  dikstra(int s) {
+        PriorityQueue<Point> pq = new PriorityQueue<>(new Comparator<Point>(){
             @Override
             public int compare(Point o1, Point o2) {
                 return o1.dist - o2.dist;
             }
         });
-
-        backPq.add(new Point(start, 0));
-        arr[start] = 0;
-
-        while (!backPq.isEmpty()) {
-            Point cur = backPq.poll();
-            if (!visited[cur.v]) {
-                visited[cur.v] =true;
-                for (Point next : graph[cur.v]) {
+        int[] arr = new int[N+1];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        arr[s] = 0;
+        boolean[] visited = new boolean[N+1];
+        pq.offer(new Point(s, 0));
+        while(!pq.isEmpty()){
+            Point cur = pq.poll();
+            if(!visited[cur.v]){
+                visited[cur.v] = true;
+                for(Point next: graph[cur.v]){
                     if (arr[next.v] > arr[cur.v] + next.dist) {
                         arr[next.v] = arr[cur.v] + next.dist;
-                        backPq.add(new Point(next.v, arr[next.v]));
+                        pq.offer(new Point(next.v, arr[next.v]));
                     }
                 }
             }
@@ -67,11 +59,11 @@ public class Main {
         return arr;
     }
     public static class Point{
-        int v ;
+        int v;
         int dist;
 
         public Point(int v, int dist) {
-            this.v = v;
+            this.v =v ;
             this.dist = dist;
         }
     }
