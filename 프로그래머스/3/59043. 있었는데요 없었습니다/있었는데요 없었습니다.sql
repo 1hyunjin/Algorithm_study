203ce5ef4b61b1647dc51166c7789fd1215dac78
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-09-18 09:06:03 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -2,3 +2,7 @@
 select i.animal_id, i.name from animal_ins i, animal_outs o
 where i.animal_id = o.animal_id and i.datetime > o.datetime
 order by i.datetime;
+
+SELECT i.ANIMAL_ID, i.NAME FROM ANIMAL_INS i, ANIMAL_OUTS o
+WHERE i.ANIMAL_ID = o.ANIMAL_ID AND i.DATETIME > o.DATETIME
+ORDER BY i.DATETIME ASC;
