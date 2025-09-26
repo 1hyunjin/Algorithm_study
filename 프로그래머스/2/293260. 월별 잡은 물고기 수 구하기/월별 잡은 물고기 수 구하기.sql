13193160dd0983218d71a531297af6fd421d6d90
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-09-26 13:57:26 +0900

    [level 2] Title: 월별 잡은 물고기 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293260. 월별 잡은 물고기 수 구하기/월별 잡은 물고기 수 구하기.sql
+++ 프로그래머스/2/293260. 월별 잡은 물고기 수 구하기/월별 잡은 물고기 수 구하기.sql
@@ -1,6 +1,5 @@
 -- 코드를 작성해주세요
-select count(*) FISH_COUNT, MONTH(TIME) as MONTH
-from fish_info
-group by MONTH(TIME)
-having count(*) > 0
-order by MONTH(TIME) asc;
+SELECT COUNT(ID) AS FISH_COUNT, MONTH(TIME) AS MONTH
+FROM FISH_INFO
+GROUP BY MONTH(TIME)
+ORDER BY MONTH(TIME) ASC;
