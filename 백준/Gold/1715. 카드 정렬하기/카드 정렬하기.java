e516a85ae501c61d026a5f632121e29032dee3d0
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-09-26 20:44:36 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 276 ms, Memory: 25180 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,37 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o1-o2;
             }
         });
-
-        for (int i = 0; i < T; i++) {
-            int num = Integer.parseInt(br.readLine());
-            pq.offer(num);
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
         }
-
         int sum = 0;
 
         while (pq.size() > 1) {
-            Integer a = pq.poll();
-            Integer b = pq.poll();
-
-            int x = a+b;
-            sum += x;
-            pq.offer(x);
+            int first = pq.poll();
+            int second = pq.poll();
+            sum += first + second;
+            pq.offer(first+second);
         }
-
         System.out.println(sum);
     }
 }
