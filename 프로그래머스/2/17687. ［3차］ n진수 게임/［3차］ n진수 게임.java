1e2f5e0db291db11943b96e91c4b2bee888990a8
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-10-10 16:51:56 +0900

    [level 2] Title: [3차] n진수 게임, Time: 14.86 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -8,14 +8,12 @@ class Solution {
         int num = 0;
         
         while(sb.length() < m*t){
-            // System.out.println(change(num, n));
-            sb.append(change(num, n));
-            num++;
+            sb.append(Integer.toString(num++, n));
         }
         
         // System.out.println(sb.toString());
         StringBuilder result = new StringBuilder();
-        int order = 0; // 순서 (0~m-1)
+        int order = 0; 
         for(int i = 0; i < sb.length(); i++){
             if(order == p-1){
                 result.append(sb.charAt(i));
@@ -25,24 +23,7 @@ class Solution {
             }
             order = (order+1)%m;
         }
-        answer = result.toString();
+        answer = result.toString().toUpperCase();
         return answer;
-    }
-    public static String change(int num, int n){
-        if(num == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-        int x = num;
-        while(x > 0){
-            int r = x%n;
-            if(r < 10){
-                sb.append(r);
-            }
-            else{
-                sb.append((char)('A'+(r-10)));
-            }
-            x/= n;
-        }
-        
-        return sb.reverse().toString();
     }
 }
