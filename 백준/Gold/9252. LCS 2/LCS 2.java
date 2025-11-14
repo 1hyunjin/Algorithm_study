140ce00aabea34b9436eefe2a8ce6c24697138ca
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-11-14 18:03:59 +0900

    [Gold IV] Title: LCS 2, Time: 108 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,41 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s1 = br.readLine();
-
         String s2 = br.readLine();
 
         int[][] dp = new int[s1.length()+1][s2.length()+1];
 
         for (int i = 1; i <= s1.length(); i++) {
             for (int j = 1; j <= s2.length(); j++) {
-                // 문자가 일치한다면?
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
-                // 일치하지 않는다면?
-                else {
+                else{
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(dp[s1.length()][s2.length()]).append("\n");
-
-        int answer = dp[s1.length()][s2.length()];
-        char[] result = new char[answer];
-        int idx = 0;
-
+        int max = dp[s1.length()][s2.length()];
+        System.out.println(max);
+        char[] result = new char[max];
         int i = s1.length();
         int j = s2.length();
-
+        int idx = 0;
         while (true) {
-            if (idx == answer) {
+            if (idx == max) {
                 break;
             }
             if (dp[i][j] == 0) {
@@ -45,18 +36,18 @@ public class Main {
                 i = i-1;
             } else if (dp[i][j] == dp[i][j - 1]) {
                 j = j-1;
-            } else {
+            }
+            else{
                 result[idx] = s1.charAt(i-1);
                 i = i-1;
                 j = j-1;
                 idx++;
             }
         }
-
+        StringBuilder sb = new StringBuilder();
         for (int s = result.length - 1; s >= 0; s--) {
             sb.append(result[s]);
         }
-
         System.out.println(sb.toString());
     }
 }
