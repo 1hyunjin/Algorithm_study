064d661ebf343cac6c947d1e6567eb30f891fbd4
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-11-14 17:56:24 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,25 +1,20 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s1 = br.readLine();
-
         String s2 = br.readLine();
 
         int[][] dp = new int[s1.length()+1][s2.length()+1];
 
         for (int i = 1; i <= s1.length(); i++) {
             for (int j = 1; j <= s2.length(); j++) {
-                // 문자가 일치한다면?
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
-                // 일치하지 않는다면?
-                else {
+                else{
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
