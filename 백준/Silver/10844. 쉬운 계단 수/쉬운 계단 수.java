ec3ff117ae8ecd511462f834f6f7b63b836fd462
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-12-02 14:56:34 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -2,30 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int mod = 1000000000;
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N + 1][10];
-        Arrays.fill(dp[1], 1);
+        int INF = 1000000000;
+        int[][] dp = new int[101][10];
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
         for (int i = 2; i <= N; i++) {
             for (int j = 0; j <= 9; j++) {
                 if (j == 0) {
-                    dp[i][0] = dp[i - 1][1] % mod;
+                    dp[i][0] = dp[i-1][1]%INF;
                 }
                 else if (j == 9) {
-                    dp[i][9] = dp[i - 1][8] % mod;
+                    dp[i][9] = dp[i-1][8]%INF;
                 }
                 else{
-                    dp[i][j] = (dp[i-1][j-1]%mod + dp[i-1][j+1]%mod)%mod;
+                    dp[i][j] = (dp[i-1][j-1]+ dp[i-1][j+1])%INF;
                 }
             }
         }
         int sum = 0;
-        for (int i = 1; i <= 9; i++) {
-            sum+=dp[N][i];
-            sum%=mod;
+        for (int i = 0; i <= 9; i++) {
+            sum = (sum+dp[N][i])%INF;
         }
-        System.out.println(sum%mod);
+        System.out.println(sum);
     }
 }
