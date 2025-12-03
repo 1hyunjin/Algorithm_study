857edc1b73cd2df4393996c9002ddbd2d2094c6f
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-12-03 09:09:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 416 ms, Memory: 54988 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,32 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         String[] arr = new String[N+1];
-
         for (int i = 1; i <= N; i++) {
             String name = br.readLine();
-            map.put(name, i );
             arr[i] = name;
+            map.put(name, i);
         }
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            String question = br.readLine();
-
-            if (map.containsKey(question)) {
-                System.out.println(map.get(question));
+            String s = br.readLine();
+            if (map.containsKey(s)) {
+                sb.append(map.get(s)).append("\n");
             }
-            else {
-                System.out.println(arr[Integer.parseInt(question)]);
+            else{
+                sb.append(arr[Integer.parseInt(s)]).append("\n");
             }
         }
+        System.out.println(sb.toString());
     }
 }
