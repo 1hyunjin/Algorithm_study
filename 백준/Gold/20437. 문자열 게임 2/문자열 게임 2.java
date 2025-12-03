2f44f61cec2aba4b8c898f2aa90fde023a0df596
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2025-12-03 15:17:06 +0900

    [Gold V] Title: 문자열 게임 2, Time: 372 ms, Memory: 21060 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -2,41 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
-            String s = br.readLine();
+           String s = br.readLine();
             int K = Integer.parseInt(br.readLine());
-
-            if (K == 1) {
-                sb.append("1 1").append("\n");
-                continue;
-            }
-            int[] alpha = new int[26];
-            for (int i = 0; i < s.length(); i++) {
+           int len = s.length();
+           int[] alpha = new int[26];
+            for (int i = 0; i < len; i++) {
                 alpha[s.charAt(i)-'a']++;
             }
-            int min = Integer.MAX_VALUE;
             int max = -1;
-            for (int i = 0; i < s.length(); i++) {
-                if(alpha[s.charAt(i)-'a'] < K) continue;
-
-                int cnt = 1;
-
-                for (int j = i + 1; j < s.length(); j++) {
-                    if(s.charAt(i)==s.charAt(j)) cnt++;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < len; i++) {
+                char c = s.charAt(i);
+                if(alpha[c-'a'] < K) continue;
+                int cnt = 0;
+                for (int j = i; j < len; j++) {
+                    if(c == s.charAt(j)) cnt++;
                     if (cnt == K) {
-                        min = Math.min(min, j-i+1);
-                        max = Math.max(max, j-i+1);
+                        max = Math.max(max,j-i+1);
+                        min = Math.min(min, j - i + 1);
                         break;
                     }
                 }
             }
-            if (min == Integer.MAX_VALUE || max == -1) {
+            if (max == -1 || min == Integer.MAX_VALUE) {
                 sb.append(-1).append("\n");
-            } else {
+            }
+            else{
                 sb.append(min).append(" ").append(max).append("\n");
             }
         }
