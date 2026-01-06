770772617e1b9fe86f56dd19e67669a5c3ff72c4
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-06 23:09:33 +0900

    [Silver V] Title: 영화감독 숌, Time: 308 ms, Memory: 158560 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 666;
+        int cnt = 1;
+
+        while (cnt != N) {
+            num++;
+            String s = String.valueOf(num);
+            if (s.contains("666")) {
+                cnt++;
+            }
+        }
+        System.out.println(num);
+    }
+}
