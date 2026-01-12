8904a92360a14280ec1032c4cc899e6e169f7b09
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-12 09:08:31 +0900

    [Silver IV] Title: 큐, Time: 116 ms, Memory: 16252 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -5,44 +5,48 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st ;
+        StringTokenizer st;
         Deque<Integer> queue = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String op = st.nextToken();
-            if (op.equals("push") ) {
-                int x = Integer.parseInt(st.nextToken());
-                queue.offer(x);
-            } else if (op.equals("pop")) {
-                if (!queue.isEmpty()) {
-                    sb.append(queue.poll()).append("\n");
-                }
-                else{
-                    sb.append(-1).append("\n");
-                }
-            } else if (op.equals("size")) {
-                sb.append(queue.size()).append("\n");
-            } else if (op.equals("empty")) {
-                if (queue.isEmpty()) {
-                    sb.append(1).append("\n");
-                }
-                else{
-                    sb.append(0).append("\n");
-                }
-            } else if (op.equals("front")) {
-                if (!queue.isEmpty()) {
-                    sb.append(queue.peek()).append("\n");
-                }
-                else{
-                    sb.append(-1).append("\n");
-                }
-            } else {
-                int num = -1;
-                if (!queue.isEmpty()) {
-                    num = queue.peekLast();
-                }
-                sb.append(num).append("\n");
+            switch (op) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    queue.offer(x);
+                    break;
+                case "pop":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.poll()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.peek()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "back":
+                    int num = -1;
+                    if (!queue.isEmpty()) {
+                        num = queue.peekLast();
+                    }
+                    sb.append(num).append("\n");
+                    break;
             }
         }
         System.out.println(sb.toString());
