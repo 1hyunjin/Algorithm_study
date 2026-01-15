801c724c168dd5cafe8c65ddc6a12899db4ab3d8
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-15 14:29:18 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 476 ms, Memory: 65540 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,32 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         Map<String, String> map = new HashMap<>();
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String site = st.nextToken();
-            String pass = st.nextToken();
-
-            map.put(site, pass);
+            map.put(st.nextToken(), st.nextToken());
         }
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < M; i++) {
-            String find = br.readLine();
-            if (map.containsKey(find)) {
-                sb.append(map.get(find)).append("\n");
+            String site = br.readLine();
+            if (map.containsKey(site)) {
+                sb.append(map.get(site)).append("\n");
             }
         }
-        System.out.println(sb.toString());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
