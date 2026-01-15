903c976de7f542cb3fe3aec2d6d468ab55b1f884
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-15 14:48:53 +0900

    [Silver IV] Title: 암기왕, Time: 1740 ms, Memory: 384652 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,29 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            HashSet<Integer> note1 = new HashSet<>();
+            Set<Integer> set = new HashSet<>();
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-                note1.add(Integer.parseInt(st.nextToken()));
+                set.add(Integer.parseInt(st.nextToken()));
             }
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < M; i++) {
-                int num = Integer.parseInt(st.nextToken());
-                if (note1.contains(num)) {
-                    sb.append(1).append("\n");
+                if (set.contains(Integer.parseInt(st.nextToken()))) {
+                    sb.append(1).append('\n');
                 }
                 else{
-                    sb.append(0).append("\n");
+                    sb.append(0).append('\n');
                 }
             }
         }
