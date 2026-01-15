95d17a6f9dc1aa6d76e9eee187d1c6f8e7004485
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-15 14:07:49 +0900

    [Silver V] Title: 숫자 카드, Time: 764 ms, Memory: 174664 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,36 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
-
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         int N = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            map.put(Integer.parseInt(st.nextToken()), 0);
+            set.add(Integer.parseInt(st.nextToken()));
         }
-
-        int M = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (map.containsKey(num)) {
+            int n = Integer.parseInt(st.nextToken());
+            if (set.contains(n)) {
                 sb.append(1).append(" ");
             }
-            else {
+            else{
                 sb.append(0).append(" ");
             }
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
