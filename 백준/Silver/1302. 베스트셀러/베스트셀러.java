2580b605e7fadf51bb530b39d81c7ff056c3a37d
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-15 14:44:26 +0900

    [Silver IV] Title: 베스트셀러, Time: 64 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,26 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        Map<String, Integer> map  = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         int max = 0;
-
         for (int i = 0; i < N; i++) {
-            String book = br.readLine();
-            map.put(book, map.getOrDefault(book, 0) + 1);
-            max = Math.max(max, map.get(book));
+            String title = br.readLine();
+            if (map.containsKey(title)) {
+                map.put(title, map.get(title) + 1);
+            }
+            else{
+                map.put(title, 1);
+            }
+            max = Math.max(max, map.get(title));
         }
-
         List<String> list = new ArrayList<>();
-
-        for (String key : map.keySet()) {
-            if (map.get(key) == max) {
-                list.add(key);
+        for (String title : map.keySet()) {
+            if (map.get(title) == max) {
+                list.add(title);
             }
         }
         Collections.sort(list);
