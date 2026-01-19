1dfb78fb6fb6510418dbc225874c9308f4556df5
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-19 10:14:01 +0900

    [Bronze V] Title: ??!, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        sb.append(s).append("??!");
+        System.out.println(sb.toString());
+    }
+}
