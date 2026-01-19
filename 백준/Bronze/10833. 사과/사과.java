210000ad2d2df4c1f0f0fe2677c68cedb950fe59
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-19 10:12:43 +0900

    [Bronze III] Title: 사과, Time: 68 ms, Memory: 11604 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int left = b%a;
+            sum += left;
+        }
+        System.out.println(sum);
+    }
+}
