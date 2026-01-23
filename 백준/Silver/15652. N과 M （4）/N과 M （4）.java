5c368432d2511238e575c71422744748f5d0e375
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-23 15:27:39 +0900

    [Silver III] Title: N과 M (4), Time: 84 ms, Memory: 13880 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,48 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     static int N, M;
     static int[] result;
-    static int[] num;
     static StringBuilder sb;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         result = new int[M];
-        num = new int[N];
-        for (int i = 0; i < N; i++) {
-            num[i] = i+1;
-        }
         sb = new StringBuilder();
-
-        comb(0, 0);
-
+        comb(1, 0);
         System.out.println(sb.toString());
     }
 
-    public static void comb(int cnt, int idx) {
-
-        if (cnt == M) {
-            for (int i = 0; i < result.length; i++) {
-                sb.append(result[i] + " ");
+    public static void comb(int idx, int cnt) {
+        if(cnt == M){
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
         }
-
-        if (idx == N) {
-            return;
+        for (int i = idx; i <= N; i++) {
+            result[cnt] = i;
+            comb(i,cnt+1);
         }
-
-        result[cnt] = num[idx];
-        comb(cnt + 1, idx);
-        comb(cnt, idx+1);
-
     }
 }
