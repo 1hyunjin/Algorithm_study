fa1166ef22fca93a7e87543ca9e6dfb3fe5701df
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-27 09:11:59 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,67 +1,52 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N, M;
-    static int[][] arr;
-    static int[][] visited;
-
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N][M];
-        visited = new int[N][M];
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for(int j = 0; j < M; j++){
-                arr[i][j] =  str.charAt(j) - '0';
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j)-'0';
             }
         }
-
-        bfs();
-        System.out.println(visited[N-1][M-1]);
-    }
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    public static void bfs() {
-        visited[0][0] = 1;
+        boolean[][] visited = new boolean[N][M];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         Queue<Point> queue = new ArrayDeque<>();
-        queue.add(new Point(0,0));
-
+        queue.offer(new Point(0, 0));
+        visited[0][0] = true;
+        int cnt = 1;
         while (!queue.isEmpty()) {
-            Point cur = queue.poll();
-
-            for (int d = 0; d < 4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
-
-                if(nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny] == 1){
-
-                    if (visited[nx][ny] == 0) {
-                        visited[nx][ny] = visited[cur.x][cur.y] + 1;
-                        queue.add(new Point(nx, ny));
-                    }
-                    else if(visited[nx][ny] > visited[cur.x][cur.y]+1){
-                        queue.add(new Point(nx, ny));
-                        visited[nx][ny] = visited[cur.x][cur.y] + 1;
+            int size = queue.size();
+            for (int s = 0; s < size; s++) {
+                Point cur = queue.poll();
+                if (cur.x == N - 1 && cur.y == M - 1) {
+                    System.out.println(cnt);
+                    return;
+                }
+                for (int d = 0; d < 4; d++) {
+                    int nx = cur.x + dx[d];
+                    int ny = cur.y + dy[d];
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && map[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.offer(new Point(nx, ny));
                     }
                 }
             }
+            cnt++;
         }
     }
-
     public static class Point{
         int x;
         int y;
 
         public Point(int x, int y) {
-            this.x =x ;
+            this.x = x;
             this.y = y;
         }
     }
