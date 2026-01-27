f1bfbd0b18f75914e8b0d4c0457e92875fe34808
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-01-27 09:30:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 84 ms, Memory: 13844 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,69 +1,50 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
     static int N, K;
     static boolean[] visited;
-    static int answer;
-    public static void main(String[] args) throws IOException {
+    static int result;
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
         visited = new boolean[100001];
-
-        if (N == K) {
-            answer = 0;
-        } else {
-            bfs(N);
-        }
-
-        System.out.println(answer);
+        bfs(N);
     }
 
     public static void bfs(int N) {
-        Queue<Integer> queue = new ArrayDeque<>();
-
-        queue.add(N);
         visited[N] = true;
-
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(N);
         int time = 0;
-
         while (!queue.isEmpty()) {
-
             int size = queue.size();
-
             for (int s = 0; s < size; s++) {
-
                 int cur = queue.poll();
-                int left = cur - 1;
-                int right = cur + 1;
-                int jump = cur * 2;
-
-                if (left >= 0 && !visited[left]) {
-                    queue.add(left);
-                    visited[left] = true;
+                if (cur == K) {
+                    System.out.println(time);
+                    return;
                 }
-                if (right <= 100000 && !visited[right]) {
-                    queue.add(right);
-                    visited[right] = true;
+                int l = cur - 1;
+                int r = cur + 1;
+                int jump = 2 * cur;
+                if (l >= 0 && !visited[l]) {
+                    queue.offer(l);
+                    visited[l] = true;
+                }
+                if (r <= 100000 && !visited[r]) {
+                    queue.offer(r);
+                    visited[r] = true;
                 }
                 if (jump <= 100000 && !visited[jump]) {
-                    queue.add(jump);
+                    queue.offer(jump);
                     visited[jump] = true;
-                }
-
-                if (left == K || right == K || jump == K) {
-                    queue.removeAll(queue);
-                    break;
                 }
             }
             time++;
-            answer++;
         }
     }
-
 }
