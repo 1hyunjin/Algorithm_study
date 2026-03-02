c0a84f59e3ad118202bde4146ac155c27f434831
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-03-02 09:10:59 +0900

    [Silver III] Title: N과 M (5), Time: 192 ms, Memory: 31104 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,51 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int n, m;
-    static int[] nums;
-    static int[] result;
-    static boolean[] isUsed;
+class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] parent;
+    static boolean[] isSelected;
     static StringBuilder sb;
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        nums = new int[n];
-        result = new int[m];
-        isUsed = new boolean[n];
-        sb = new StringBuilder();
-
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isSelected = new boolean[N + 1];
+        arr = new int[M];
+        parent = new int[N];
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            parent[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(nums);
-        // 순열
+        Arrays.sort(parent);
+        sb = new StringBuilder();
         perm(0);
         System.out.println(sb.toString());
     }
 
     public static void perm(int cnt) {
-
-        if (cnt == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(result[i] + " ");
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
         }
-
-        for (int i = 0; i < n; i++) {
-            if (!isUsed[i]) {
-                isUsed[i] = true;
-                result[cnt] = nums[i];
+        for (int i = 0; i < N; i++) {
+            if (!isSelected[i]) {
+                isSelected[i] = true;
+                arr[cnt] = parent[i];
                 perm(cnt+1);
-                isUsed[i] = false;
+                isSelected[i] = false;
             }
         }
     }
