684ccb0c4a4af0a20cc1e5c1b57a27c9f158076b
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-03-02 09:52:07 +0900

    [Silver III] Title: N과 M (8), Time: 88 ms, Memory: 14832 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,43 +1,42 @@
 import java.io.*;
 import java.util.*;
-public class Main {
+
+class Main {
     static int N, M;
     static int[] arr;
-    static int[] result;
+    static int[] parent;
     static boolean[] isSelected;
     static StringBuilder sb;
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-        result = new int[M];
-        isSelected = new boolean[N];
-
+        isSelected = new boolean[N + 1];
+        arr = new int[M];
+        parent = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            parent[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(parent);
         sb = new StringBuilder();
         comb(0, 0);
         System.out.println(sb.toString());
     }
 
-    public static void comb(int idx, int num) {
-        if (idx == M) {
+    public static void comb(int cnt, int idx) {
+        if (cnt == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(result[i] + " ");
+                sb.append(arr[i]).append(" ");
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
         }
-        for (int i = num; i < N; i++) {
-            result[idx] = arr[i];
-            comb(idx + 1, i);
+        for (int i = idx; i < N; i++) {
+            arr[cnt] = parent[i];
+            comb(cnt + 1, i);
         }
     }
 }
