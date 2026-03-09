c6539945ea23ab014725b2d40a12e40ccf55d1de
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-03-09 19:15:32 +0900

    [Silver II] Title: 나무 자르기, Time: 624 ms, Memory: 167896 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,48 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     static int N, M;
-    static int[] trees;
+    static int[] arr;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        trees = new int[N];
         st = new StringTokenizer(br.readLine());
+        arr = new int[N];
         for (int i = 0; i < N; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(trees);
-        long s = 0;
-        long e = trees[N-1];
-        long max = trees[N-1];
-        while (s <= e) {
-            long mid = (s+e+1)/2;
-            if (cut(mid)) {
-                s = mid+1;
-                max = mid;
+        Arrays.sort(arr);
+
+        long start = 0;
+        long end = arr[N - 1];
+        long result = 0;
+        while (start <= end) {
+            long mid = (start+end+1)/2;
+            if (isCut(mid)) {
+                start = mid+1;
+                result = mid;
             }
             else{
-                e = mid-1;
+                end = mid-1;
             }
         }
-        System.out.println(max);
+        System.out.println(result);
     }
 
-    public static boolean cut(long mid) {
+    public static boolean isCut(long mid) {
         long sum = 0;
         for (int i = 0; i < N; i++) {
-            if (trees[i] >= mid) {
-                sum += trees[i] - mid;
+            if (arr[i] >= mid) {
+                sum += arr[i]-mid;
             }
         }
         if (sum >= M) {
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
 }
