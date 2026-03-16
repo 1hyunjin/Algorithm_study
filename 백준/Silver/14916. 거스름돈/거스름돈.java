54c0dae0c162696d0429b23b5c8ba68bb481cd60
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-03-16 09:16:00 +0900

    [Silver V] Title: 거스름돈, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,24 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        int sum = 0;
         while (true) {
             if (N % 5 == 0) {
-                cnt+=N/5;
+                sum += N/5;
                 break;
             }
-            N-=2;
-            cnt++;
+            N -= 2;
+            sum++;
 
             if (N < 0) {
-                cnt=-1;
+                sum = -1;
                 break;
             }
         }
-        System.out.println(cnt);
+        System.out.println(sum);
     }
 }
