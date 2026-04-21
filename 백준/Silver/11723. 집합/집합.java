cbba4aa9b5b539b0c47d46a923b0f2b300670834
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-04-21 18:51:19 +0900

    [Silver V] Title: 집합, Time: 924 ms, Memory: 324428 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,12 +1,39 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int M = Integer.parseInt(br.readLine());
+        int S = 0;
+        StringBuilder sb = new StringBuilder();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+
+            if (op.equals("all")) {
+                S = (1 << 21) - 1;  // 10000000000000000000-1 = 011111111111111111111
+            }
+            else if (op.equals("empty")) {
+                S = 0;
+            }
+            else{
+                int num = Integer.parseInt(st.nextToken());
+                if (op.equals("add")) {
+                    S |= (1 << num);
+                }
+                else if (op.equals("remove")) {
+                    S &= ~(1 << num);
+                }
+                else if (op.equals("check")) {
+                    sb.append((S & (1 << num)) != 0 ? 1 : 0).append('\n');
+                }
+                else{
+                    S ^= (1 << num);
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
