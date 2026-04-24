8188c5bc67e9491bfc3e34556cc0880259d589af
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-04-24 16:50:19 +0900

    [Silver II] Title: 부분수열의 합, Time: 176 ms, Memory: 12320 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,34 +2,33 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int N, S, cnt;
-    static int[] arr;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        arr = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int cnt = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        cnt = 0;
-        comb(0, 0);
-        if (S == 0) {
-            cnt--;
-        }
-        System.out.println(cnt);
-    }
-
-    public static void comb(int result, int idx) {
-        if (idx== N) {
-            if (result == S) {
+        for (int i = 0; i < (1 << N); i++) {
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                if ((i & (1 << j)) != 0) {
+                    sum += arr[j];
+                }
+            }
+            if (sum == S) {
                 cnt++;
             }
-            return;
         }
-        comb(result + arr[idx], idx + 1);
-        comb(result, idx+1);
+        if (S == 0) {
+            System.out.println(cnt-1);
+        }
+        else{
+            System.out.println(cnt);
+        }
     }
 }
