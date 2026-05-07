d30dca753a921695d59b84f5fe6900b6e9a4733a
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-05-07 15:00:19 +0900

    [level 1] Title: 폰켓몬, Time: 3.83 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+       Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        if(map.size() >= nums.length/2){
+            return nums.length/2;
+        }
+        else{
+            return map.size();
+        }
+    }
+}
