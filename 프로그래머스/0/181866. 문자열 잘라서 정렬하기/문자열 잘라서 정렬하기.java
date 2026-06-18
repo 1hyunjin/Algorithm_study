29455b77824fd4984cede168e9304d531b275ac6
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-06-18 17:51:41 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 10.73 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String myString) {
+        String[] answer = myString.split("x");
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < answer.length; i++){
+            if(!answer[i].equals("")){
+                list.add(answer[i]);
+            }
+        }
+        Collections.sort(list);
+        return list.toArray(new String[0]);
+    }
+}
