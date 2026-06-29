ff59ba3fa319e1be58915b23d8c5ab2d2fc44d3c
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-06-29 10:06:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.72 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String s = ""+n;
+        
+        for(int i = 0; i < s.length(); i++){
+            int num = s.charAt(i)-'0';
+            answer += num;
+        }
+
+        return answer;
+    }
+}
