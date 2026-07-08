5428e9cdd9ae59689179d10b7498d6cb646c5371
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-07-08 10:22:03 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.15 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,42 +1,42 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        
+        int[] answer = {};
         Map<String, Integer> map = new HashMap<>();
         for(int i = 0; i < terms.length; i++){
             String[] arr = terms[i].split(" ");
             map.put(arr[0], Integer.parseInt(arr[1]));
         }
+        // System.out.println(map);
         List<Integer> list = new ArrayList<>();
         String[] todays = today.split("\\.");
         int tyear = Integer.parseInt(todays[0]);
         int tmonth = Integer.parseInt(todays[1]);
         int tday = Integer.parseInt(todays[2]);
-        int toSum = (tyear * 12 * 28) + (tmonth * 28) + tday;
-        
+        int tsum = (tyear*12*28) + tmonth*28 + tday;
+        // System.out.println(tsum);
+        // System.out.println("=========");
         for(int i = 0; i < privacies.length; i++){
             String[] arr = privacies[i].split(" ");
-            String person = arr[1];
-            String last = arr[0];
+            String kinds = arr[1]; // 종류
             
-            String[] dates = last.split("\\.");
-            int year = Integer.parseInt(dates[0]);
-            int month = Integer.parseInt(dates[1]) + map.get(person);
-            int day = Integer.parseInt(dates[2]);
+            String[] days = arr[0].split("\\.");
+            int year = Integer.parseInt(days[0]);
+            int month = Integer.parseInt(days[1]);
+            int day = Integer.parseInt(days[2]);
             
-            int total = (year * 12 * 28) + (month * 28) + day -1;
+            int kind = map.get(kinds);
             
-            if(total < toSum){
+            int sum = (year*28*12) + ((month+kind)*28) + day -1;
+            // System.out.println(sum);
+            if(sum < tsum){
                 list.add(i+1);
             }
         }
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
+        answer = new int[list.size()];
+        for(int i =0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
-        
         return answer;
     }
 }
