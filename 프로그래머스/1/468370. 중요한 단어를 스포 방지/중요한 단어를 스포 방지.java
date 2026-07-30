eba5d4aa3c843885eb926767c0256c8630672a94
HyunjinYoo <38430900+1hyunjin@users.noreply.github.com>
2026-07-30 13:12:33 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 66.53 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Solution {
+    static String tmp;
+    static String message;
+    public int solution(String message, int[][] spoiler_ranges) {
+        this.message = message;
+        String[] messages = message.split(" ");
+        
+        tmp = message;
+        for(int i = 0; i < spoiler_ranges.length; i++){
+            int start = spoiler_ranges[i][0];
+            int end = spoiler_ranges[i][1];
+            
+            makeSpoiler(start, end);
+        }
+        String[] tmpArr = tmp.split(" ");
+        
+        // Map<String, Integer> words = new HashMap<>(); // 스포가 아닌 단어
+        // Map<String, Integer> spoilers = new HashMap<>(); // 스포인 단어
+        Set<String> words = new HashSet<>();
+        Set<String> spoilers = new HashSet<>();
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < tmpArr.length; i++){
+            String s = tmpArr[i];
+            if(s.contains("*")){
+                spoilers.add(messages[i]);
+                list.add(messages[i]);
+            }
+            else{
+                words.add(messages[i]);
+            }
+        }
+        // System.out.println(words);
+        // System.out.println(spoilers);
+        
+        Set<String> importantWords = new HashSet<>(); // 중요한 단어 
+        for(int i = 0; i < list.size(); i++){
+            String word = list.get(i);
+            // 메시지의 스포 방지 구간이 아닌 구간에 등장한 적이 없어야 한다. 
+            // 이전에 공개된 스포 방지 단어와 중복되면 안된다.
+            if(words.contains(word)) continue;
+            if(importantWords.contains(word)) continue;
+            importantWords.add(word);
+        }
+        // System.out.println("--------------------");
+        // for(String val : importantWords){
+        //     System.out.println(val);
+        // }
+        int answer = importantWords.size();
+        return answer;
+    }
+    public static void makeSpoiler(int start, int end){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < tmp.length(); i++){
+            char c = tmp.charAt(i);
+            if(i >= start && i <= end){
+                if(Character.isDigit(c) || Character.isAlphabetic(c)){
+                    sb.append("*");
+                }
+                else if(c == '*'){
+                    sb.append("*");
+                }
+                else{
+                    sb.append(" ");
+                }
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        tmp = sb.toString();
+        // System.out.println(tmp);
+    }
+}
